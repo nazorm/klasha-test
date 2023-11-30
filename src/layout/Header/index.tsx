629a6e4dd819cb93bba68489893f4fed16f167f0
Chinazor Nnadozie <nnadoziechinazor@gmail.com>
2023-11-30 16:22:41 +0100

    iimplements header

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import userIcon from '../../assets/user.svg';
+import dropdownIcon from '../../assets/dropdown.svg'
+import { Dropdown } from 'antd';
+import { Select } from 'antd';
+const languageOptions = [
+    {
+        label: 'En',
+        key: '1'
+    },
+]
+
+
+const items = [
+    {
+        label: 'User Center',
+        key: '1'
+    },
+    {
+        label: 'Logout',
+        key: '2'
+    },
+]
+
+
+export const Header = () => {
+    const [languageValue, setLanguageValue] = useState('En')
+
+    const handleLanguageChange = (value: string) => {
+        setLanguageValue(value);
+    };
+
+
+    return (
+        <Wrapper>
+            <div className='header-content'>
+                <Dropdown menu={{ items }} className='user' >
+                    <img src={userIcon} alt='user' title='user' className='header-icon' />
+                </Dropdown>
+                <Select
+                    suffixIcon={<img src={dropdownIcon} alt='drop-down' title='drop-down' className='header-icon' />}
+                    value={languageValue}
+                    style={{ width: 60 }}
+                    onChange={handleLanguageChange}
+                    options={languageOptions}
+                />
+            </div>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+  border: 1px solid blue;
+  display: flex;
+  justify-content: flex-end;
+  padding: 2rem;
+  .header-content{
+    display:flex;
+    align-items: center;
+  }
+  .user{
+    margin-right: 20px;
+  }
+`;
